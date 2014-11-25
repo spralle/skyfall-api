6c8700680f6b80091de01b648f0d444b756e515c
unknown <emil@andreas10.west.surikat.net>
2014-11-25 18:09:39 +0100

    ...

--- test/api_bridge.js
+++ test/api_bridge.js
@@ -6,10 +6,19 @@ var request = require('supertest');
 var Registry = require('..').Registry;
 var Bridge = require('..').Bridge;
 
-describe('api_bridge', function()
-{
-  it('should return 404 when no method exists', function(done)
-  {
+describe('api_bridge', function() {
+  it('should return 404 when no version exists', function(done) {
+    var registry = new Registry();
+    //add version but not method
+    var app = express();
+    app.use('/api', new Bridge(registry).getHandler());
+    request(app)
+      .get('/api/v1/mytestmethod')
+      .expect(404, done);
+  });
+
+
+  it('should return 404 when no method exists', function(done) {
     var registry = new Registry();
     //add version but not method
     registry.versions('v1');
@@ -20,26 +29,39 @@ describe('api_bridge', function()
       .expect(404, done);
   });
 
-  it('should return 200 when method exists', function(done)
-  {
+  it('should return 200 when method exists', function(done) {
     var registry = new Registry();
-    registry.versions('v1').get('/mytestmethod',
-    {
+    registry.versions('v1').get('/mytestmethod', {
       description:"Test Method",
       summary:"Summary of Test Method",
       type:'string',
       parameters:[]
     },
-    function(req, res)
-    {
+    function(req, res) {
       res.send('OK');
     });
     var app = express();
     app.use('/api', new Bridge(registry).getHandler());
     request(app)
       .get('/api/v1/mytestmethod')
-      .expect(404, done);
+      .expect(200, done);
   });
-
+  it('should return correct data when invoked', function(done) {
+    var registry = new Registry();
+    registry.versions('v1').get('/mytestmethod', {
+      description:"Test Method",
+      summary:"Summary of Test Method",
+      type:'string',
+      parameters:[]
+    },
+    function(req, res) {
+      res.send('OK');
+    });
+    var app = express();
+    app.use('/api', new Bridge(registry).getHandler());
+    request(app)
+      .get('/api/v1/mytestmethod')
+      .expect(200, 'OK', done);
+  });
 
 });
--- test/register_method.js
+++ test/register_method.js
@@ -3,7 +3,7 @@ var ApiService = require('..').Registry;
 
 describe('api_service', function()
 {
-  describe('#registerMethod', function()
+  describe('#get', function()
   {
     it('should return true if method registered', function()
     {
@@ -26,6 +26,111 @@ describe('api_service', function()
         }
       );
       manager.getRoutes()[0].path.should.equal('/carrier/match');
+      manager.getRoutes()[0].method.should.equal('get');
     })
   })
+describe('#post', function()
+{
+  it('should return true if method is post', function()
+  {
+    var api = new ApiService();
+    var manager = api.versions('v1');
+    manager.post('/carrier/match',
+    {
+      description:"Match a carrier vs from and to location",
+      summary:'Summary for carrier matching',
+      type:'array',
+      items:{$ref:'Party'},
+      parameters:[
+        ApiService.parameterTypes.query('from', 'From Address', 'string'),
+        ApiService.parameterTypes.query('to', 'To Address', 'string')
+      ]
+    },
+      function(req, res)
+      {
+        res.send('OK');
+      }
+    );
+    manager.getRoutes()[0].path.should.equal('/carrier/match');
+    manager.getRoutes()[0].method.should.equal('post');
+  })
+})
+describe('#put', function()
+{
+  it('should return true if method is put', function()
+  {
+    var api = new ApiService();
+    var manager = api.versions('v1');
+    manager.put('/carrier/match',
+    {
+      description:"Match a carrier vs from and to location",
+      summary:'Summary for carrier matching',
+      type:'array',
+      items:{$ref:'Party'},
+      parameters:[
+        ApiService.parameterTypes.query('from', 'From Address', 'string'),
+        ApiService.parameterTypes.query('to', 'To Address', 'string')
+      ]
+    },
+      function(req, res)
+      {
+        res.send('OK');
+      }
+    );
+    manager.getRoutes()[0].path.should.equal('/carrier/match');
+    manager.getRoutes()[0].method.should.equal('put');
+  })
+})
+describe('#patch', function()
+{
+  it('should return true if method is patch', function()
+  {
+    var api = new ApiService();
+    var manager = api.versions('v1');
+    manager.patch('/carrier/match',
+    {
+      description:"Match a carrier vs from and to location",
+      summary:'Summary for carrier matching',
+      type:'array',
+      items:{$ref:'Party'},
+      parameters:[
+        ApiService.parameterTypes.query('from', 'From Address', 'string'),
+        ApiService.parameterTypes.query('to', 'To Address', 'string')
+      ]
+    },
+      function(req, res)
+      {
+        res.send('OK');
+      }
+    );
+    manager.getRoutes()[0].path.should.equal('/carrier/match');
+    manager.getRoutes()[0].method.should.equal('patch');
+  })
+})
+describe('#delete', function()
+{
+  it('should return true if method is delete', function()
+  {
+    var api = new ApiService();
+    var manager = api.versions('v1');
+    manager.delete('/carrier/match',
+    {
+      description:"Match a carrier vs from and to location",
+      summary:'Summary for carrier matching',
+      type:'array',
+      items:{$ref:'Party'},
+      parameters:[
+        ApiService.parameterTypes.query('from', 'From Address', 'string'),
+        ApiService.parameterTypes.query('to', 'To Address', 'string')
+      ]
+    },
+      function(req, res)
+      {
+        res.send('OK');
+      }
+    );
+    manager.getRoutes()[0].path.should.equal('/carrier/match');
+    manager.getRoutes()[0].method.should.equal('delete');
+  })
+})
 });
